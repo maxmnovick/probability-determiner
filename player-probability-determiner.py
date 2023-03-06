8689c897c49c12766729bdaed1afea8e2ca41372
maxmnovick <maxmnovick@gmail.com>
2023-03-06 02:07:39 -0500

    enabled plot of psat 3 mean

--- player-probability-determiner.py
+++ player-probability-determiner.py
@@ -1575,6 +1575,62 @@ for valid_streak in all_valid_streaks_list:
     plt.plot(game_nums, stat_vals, label = "Stat Vals") # reverse bc input from recent to distant but we plot left to right
     plt.plot(game_nums, plot_stat_line,  label = "Stat Line")
 
+    # also plot avg over time to compare trend of avg
+    # bc just seeing season avg is barely useful almost useless unless we see either avg in last few games (and multiple subset) or we can simply see if avg is increasing or decreasing
+    # the avg for the first game must be based on previous seasons
+    # but for now arbitrary number
+    #init_mean_stat_val
+    prev_stat_vals = []
+    mean_stat_vals = [] # how mean changes over time
+    past_ten_stat_vals = []
+    past_ten_mean_stat_vals = [] # mean over last 10 games to get more recent relevant picture
+    past_three_stat_vals = []
+    past_three_mean_stat_vals = []
+    for stat_val_idx in range(len(stat_vals)):
+        stat_val = stat_vals[stat_val_idx]
+        print('prev_stat_vals: ' + str(prev_stat_vals))
+        print('past_ten_stat_vals: ' + str(past_ten_stat_vals))
+        # compute avg of this and previous vals
+        if stat_val_idx == 0:
+            mean_stat_val = stat_val
+            past_ten_mean_stat_val = stat_val
+            past_three_mean_stat_val = stat_val
+
+        else:
+            mean_stat_val = round(numpy.mean(prev_stat_vals), 1)
+            print('mean_stat_val: ' + str(mean_stat_val))
+            past_ten_mean_stat_val = round(numpy.mean(past_ten_stat_vals), 1)
+            print('past_ten_mean_stat_val: ' + str(past_ten_mean_stat_val))
+            past_three_mean_stat_val = round(numpy.mean(past_three_stat_vals), 1)
+            print('past_three_mean_stat_val: ' + str(past_three_mean_stat_val))
+
+        mean_stat_vals.append(mean_stat_val)
+        past_ten_mean_stat_vals.append(past_ten_mean_stat_val)
+        past_three_mean_stat_vals.append(past_three_mean_stat_val)
+
+        prev_stat_vals.append(stat_val)
+
+        if stat_val_idx < 10: # add vals to list until we reach 10 bc we only want past 10 games
+            past_ten_stat_vals.append(stat_val)
+        else: # replace in list instead of adding
+            past_ten_stat_vals.pop(0)
+            past_ten_stat_vals.append(stat_val)
+        if stat_val_idx < 3: # add vals to list until we reach 10 bc we only want past 10 games
+            past_three_stat_vals.append(stat_val)
+        else: # replace in list instead of adding
+            past_three_stat_vals.pop(0)
+            past_three_stat_vals.append(stat_val)
+        
+
+    print('mean_stat_vals: ' + str(mean_stat_vals))
+    print('past_ten_mean_stat_vals: ' + str(past_ten_mean_stat_vals))
+    print('past_three_mean_stat_vals: ' + str(past_three_mean_stat_vals))
+
+    plt.plot(game_nums, mean_stat_vals,  label = "Overall Mean")
+    plt.plot(game_nums, past_ten_mean_stat_vals,  label = "Past 10 Mean")
+    plt.plot(game_nums, past_three_mean_stat_vals,  label = "Past 3 Mean")
+
+
     plt.title(player_name.title() + " " + stat_name.upper() + " over Time")
     plt.xlabel("Game Num")
     plt.ylabel(stat_name.upper())
